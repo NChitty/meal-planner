86c16fa767729ac713bc7ac5aafe4fc6ab629c16
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-11-17 08:55:23 -0500

    CDK pipelines GH

--- cdk/lib/infrastructure-stack.ts
+++ cdk/lib/infrastructure-stack.ts
@@ -1,21 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
-import {Construct} from 'constructs';
-import {aws_s3 as s3} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { AwsCredentials, GitHubActionRole, GitHubWorkflow } from 'cdk-pipelines-github';
+import { ShellStep } from 'aws-cdk-lib/pipelines';
 
 /**
  * Stack for deploying meal planner app.
  */
 export default class MealPlannerStack extends cdk.Stack {
   /**
-     * Construct a meal planner stack.
-     * @param{Construct} scope not sure.
-     * @param{string} id idk.
-     * @param{cdk.StackProps} props the last thing?
-     */
+   * Construct a meal planner stack.
+   * @param{Construct} scope not sure.
+   * @param{string} id idk.
+   * @param{cdk.StackProps} props the last thing?
+   */
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
+    const provider = new GitHubActionRole(this, 'github-action-role', {
+      repos: ['NChitty/meal-planner'],
+    });
+    const pipeline = new GitHubWorkflow(this, 'Pipeline', {
+      synth: new ShellStep('Build', {
+        commands: [
+          'npm install',
+          'npm build',
+        ],
+      }),
+      awsCreds: AwsCredentials.fromOpenIdConnect({
+        gitHubActionRoleArn: provider.role.roleArn,
+      }),
     });
   }
 }

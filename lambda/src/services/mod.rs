e721acc29e93010080dc484d1dafdb6b41420f11
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-01-15 16:25:15 -0500

    With context

--- lambda/src/services/mod.rs
+++ lambda/src/services/mod.rs
@@ -1,8 +1,20 @@
 use axum::Router;
 use axum::routing::get;
+use crate::services::recipes::TestRecipeRepository;
 
 mod recipes;
+
+#[derive(Clone)]
+struct ApplicationContext<T> {
+    pub repo: T,
+}
+
 pub fn recipes() -> Router {
+    let recipe_context = ApplicationContext {
+        repo: TestRecipeRepository {}
+    };
+
     Router::new()
-        .route("/:id", get(recipes::read_one))
+        .route("/:id", get(recipes::read_one::<TestRecipeRepository>))
+        .with_state(recipe_context)
 }
--- lambda/src/services/recipes.rs
+++ lambda/src/services/recipes.rs
@@ -1,10 +1,34 @@
-use axum::extract::Path;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::Json;
 use uuid::Uuid;
 
 use crate::recipe::Recipe;
+use crate::services::ApplicationContext;
+use crate::Repository;
 
-pub(super) async fn read_one(Path(_): Path<Uuid>) -> Result<Json<Recipe>, StatusCode> {
-    Ok(Json(Recipe::default()))
+#[derive(Clone)]
+pub(super) struct TestRecipeRepository {}
+
+impl Repository<Recipe> for TestRecipeRepository {
+    fn find_by_id(&self, id: Uuid) -> Option<Recipe> {
+        if id.is_nil() {
+            return Some(Recipe::default());
+        }
+        None
+    }
+}
+
+pub(super) async fn read_one<T>(
+    State(state): State<ApplicationContext<T>>,
+    Path(id): Path<Uuid>,
+) -> Result<Json<Recipe>, StatusCode>
+where
+    T: Repository<Recipe>,
+{
+    if let Some(recipe) = state.repo.find_by_id(id) {
+        return Ok(Json(recipe));
+    }
+
+    Err(StatusCode::NOT_FOUND)
 }

db415d072808adab8ef483d06e56fc34453a09c3
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-03-01 17:00:19 -0500

    Must define environment

--- cdk/bin/meal-planner.ts
+++ cdk/bin/meal-planner.ts
@@ -2,6 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import PipelineStack from '../lib/pipeline';
+import * as pipeline from '../lib/pipeline';
 
 const app = new cdk.App();
-new PipelineStack(app, 'MealPlannerPipeline');
+new PipelineStack(app, 'MealPlannerPipeline', {
+  env: pipeline.stagingEnvironment,
+});

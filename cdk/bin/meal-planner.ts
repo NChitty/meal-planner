9ffa7c4c12021c942ee04f6647a3804253b317f0
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-03-01 16:57:22 -0500

    Use pipeline stack in app

--- cdk/bin/meal-planner.ts
+++ cdk/bin/meal-planner.ts
@@ -1,16 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import MealPlannerStage from '../lib/application-stage';
+import PipelineStack from '../lib/pipeline';
 
-export const stagingEnvironment: cdk.Environment = {
-  account: '381491920629',
-  region: 'us-east-1',
-};
-
-export const prodEnvironment: cdk.Environment = {
-  account: '654654503876',
-  region: 'us-east-1',
-};
 const app = new cdk.App();
-new MealPlannerStage(app, 'MealPlannerApp', { env: prodEnvironment });
+new PipelineStack(app, 'MealPlannerPipeline');

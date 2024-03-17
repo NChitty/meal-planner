53a96dbb40397c83d987b3e7bfa660c8129b1611
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-03-17 13:32:36 -0400

    Create recipe function

--- lambda/src/services/recipes.rs
+++ lambda/src/services/recipes.rs
@@ -3,6 +3,7 @@ use axum::http::StatusCode;
 use axum::Json;
 use uuid::Uuid;
 
+use crate::recipe::request_models::CreateRecipe;
 use crate::recipe::Recipe;
 use crate::services::ApplicationContext;
 use crate::Repository;
@@ -15,6 +16,19 @@ where
     T: Repository<Recipe>,
 {
     let recipe = state.repo.find_by_id(id).await?;
+
+    Ok(Json(recipe))
+}
+
+pub(super) async fn create<T>(
+    State(state): State<ApplicationContext<T>>,
+    Json(payload): Json<CreateRecipe>,
+) -> Result<Json<Recipe>, StatusCode>
+where
+     T: Repository<Recipe>
+{
+    let recipe = Recipe::create_new(Uuid::new_v4(), &payload);
+    state.repo.save(&recipe).await?;
 
     Ok(Json(recipe))
 }

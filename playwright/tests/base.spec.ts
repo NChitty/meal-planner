18bd371a60f11dbefba98b320472f9e0d7aa3e5a
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-04-01 09:38:28 -0400

    Remove root route

--- playwright/tests/base.spec.ts
+++ playwright/tests/base.spec.ts
@@ -1,26 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('pingable', async ({ request }) => {
-  const response = await request.get('/ping');
+  const response = await request.get('./ping');
 
   expect(response.ok()).toBeTruthy();
-});
-
-test('hello world', async ({ request }) => {
-  const response = await request.get('');
-
-  expect(response.ok()).toBeTruthy();
-  expect(await response.json()).toEqual({ msg: 'Hello world!' });
-});
-
-test('hello name', async ({ request }) => {
-  const name = 'Nicholas';
-  const response = await request.get('', {
-    params: {
-      'name': name,
-    },
-  });
-
-  expect(response.ok()).toBeTruthy();
-  expect(await response.json()).toEqual({ msg: `Hello ${name}!` });
+  expect(await response.json()).toEqual({ msg: 'Pong' });
 });
